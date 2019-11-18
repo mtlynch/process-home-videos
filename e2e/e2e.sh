2f8266df74d24f2ac091ac0e4cfed00895d34e83
Michael Lynch <git@mtlynch.io>
2019-11-17 21:03:35 -0500

    Switching to bash script

---
+++ e2e/e2e.sh
@@ -0,0 +1,16 @@
+set -xe
+
+sudo apt-get update
+sudo apt-get install \
+  ffmpeg \
+  python3 \
+  python3-venv \
+  --yes
+python3 --version
+python3 -m venv ./venv
+. venv/bin/activate
+pip install --requirement requirements.txt
+./e2e/test-csv-to-yaml
+VIDEOS_DIR=$(mktemp -d)
+./e2e/test-render-scenes "$VIDEOS_DIR"
+./e2e/test-publish-to-mediagoblin "$VIDEOS_DIR"
